7c311765438db319ea20dd6b5a91753353e76e5b
chris72205 <chris72205@users.noreply.github.com>
2015-09-26 19:52:34 -0500

    add config file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+//Instapaper API
+define("CONSUMERKEY", "");
+define("CONSUMERSECRET", "");
+
+define("OAUTHTOKEN", "");
+define("OAUTHSECRET", "");
+
+define("SENDFOLDERID", "");
+define("SENTFOLDERID", "");
+
+//=============================
+
+//Mailgun API
+define("MAILGUNKEY", "");
+define("DOMAIN", "");
